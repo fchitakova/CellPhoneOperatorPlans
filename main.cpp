d0f28f391c5f47eba5c1e9c918f84a6b3180951b
fchitakova <fchitakova@users.noreply.github.com>
2017-03-11 16:13:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-#include "Header.h"
+#include "Mentel.h"
 
 
 int main()
 {
 	int n;
+	unsigned wantedMins, wantedSMS, wantedMB, wantedTerm;
+	double preferedPrice;
 
 	std::cout << "Number of plans: ";
 
@@ -13,22 +15,47 @@ int main()
 		std::cin >> n;
 	} while (n < 1);
 
+
+
 	TariffPlan*plans = new TariffPlan[n];
 
 	readArrayOfPlans(plans, n);
 
-	sortTarrifPlansByPrice(plans, n);
+	std::cout << "Please enter how many minutes,SMSs and MBs you need at least: ";
 
-	printAllPlans(plans, n);
+	std::cin >> wantedMins >> wantedSMS >> wantedMB;
 
-	filterPlansByPreference(plans, n);
+	std::cout << '\n';
+
+	std::cout << "Please enter how long term do you prefer: ";
+
+	std::cin >> wantedTerm;
+
+	std::cout << '\n' << "Please enter the max price that satisfies you: ";
+
+	std::cin >> preferedPrice;
+
+	size_t newsize = 0;
+
+    TariffPlan*filteredPlans=filterPlansByPreference(plans,n, wantedMins, wantedSMS, wantedMB, wantedTerm, preferedPrice,newsize);
+
+	sortTarrifPlansByPrice(filteredPlans, n);
+
+	std::cout << '\n';
+
+	std::cout << "The plans ,which satisfies your preferences are: ";
+
+	std::cout << '\n';
+
+	printAllPlans(filteredPlans, newsize);
 
 
 
-
-
-
+	delete[]filteredPlans;
 	delete[]plans;
+
+	plans = 0;
+	filteredPlans = 0;
 
 	return 0;
 }
